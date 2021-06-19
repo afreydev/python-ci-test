7b69ca54fdf4c6eae50c8481ae794c72e40f5cc2
Angel Rey <afreydev@gmail.com>
2021-06-19 08:50:02 -0500

    Added linter

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -6,8 +6,9 @@ def test_print_message():
     formatted_message = f"This is printing this: {message}"
     assert formatted_message == printer.print_this_message(message)
 
+
 def test_add_message():
     num1 = 3
     num2 = 2
-    formatted_message = f"Result: 5"
+    formatted_message = "Result: 5"
     assert formatted_message == printer.print_add(num1, num2)
