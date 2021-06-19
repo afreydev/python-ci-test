b0cdb6a54c2cc960377ee03ec1b2db38db6fdaed
Angel Rey <afreydev@gmail.com>
2021-06-19 08:53:51 -0500

    Fixed bug

--- printer.py
+++ printer.py
@@ -3,6 +3,7 @@ def print_this_message(message: str):
     print(formatted_message)
     return formatted_message
 
+
 def print_add(num1: int, num2: int):
     res = num1 + num2
     formatted_message = "Result: {}".format(str(res))
